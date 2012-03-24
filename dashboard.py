a7f0e646edbb9edb78ae0f93f3e695d8fb5ed220
Andy Hume <andyhume@gmail.com>
2012-03-24 18:45:11 +0000

    Refactor dashboard view to use new TestResult classes.

--- dashboard.py
+++ dashboard.py
@@ -10,6 +10,7 @@ from django.utils import simplejson as json
 
 import models
 from utils import xml_to_json, get_urls
+from testresult import GoogleResultData, WptResultData, NoDataError
 
 class DashboardHandler(webapp.RequestHandler):
 
@@ -17,7 +18,6 @@ class DashboardHandler(webapp.RequestHandler):
     	return models.TestResult.all().filter('url =', url).filter('provider =', provider).filter('results_received', True).order('-dt')
 
     def get(self):
-
         urls = get_urls()
         context = {
             'results': []
@@ -25,51 +25,13 @@ class DashboardHandler(webapp.RequestHandler):
         
         for url in urls:
             try:
-            	g_results = self.get_test_results('google', url)
-            	wpt_results = self.get_test_results('wpt', url)
-            except:
-                self.response.out.write("Some test urls don't have even one result yet, so let's just bail out. Early days.")
-                return
-
-            result = {
-                'google': json.loads(g_results.fetch(1)[0].result),
-                'wpt': xml_to_json(wpt_results.fetch(1)[0].result)
-            }
-            
-            # Get the last 10 automated page scores to graph.
-            score_history = [json.loads(load.result)['score'] for load in g_results.filter('auto =', True).fetch(10)]
-
-            # Get the last 10 automated PLTs to graph.
-            history = wpt_results.filter('auto =', True).fetch(10)
-            plt_history = []
-            for load in history:
-                parsed = xml_to_json(load.result)
-                plt_history.append({
-                    'time': load.dt,
-                    'plt1': parsed['data']['median']['firstView']['loadTime'],
-                    'plt2': parsed['data']['median']['repeatView']['loadTime'],
-                    'render1': parsed['data']['median']['firstView']['render'],
-                    'render2': parsed['data']['median']['repeatView']['render'],
-                    'score': score_history.pop(0)
-                })
-            plt_history.reverse()
-
-            # Get the top five rules by impact.
-            rules_by_impact = []
-            for rule_result in result['google']['formattedResults']['ruleResults'].values():
-                rules_by_impact.append({
-                    'name': rule_result['localizedRuleName'],
-                    'impact': rule_result['ruleImpact']
-                })
-            rules_by_impact = sorted(rules_by_impact, key=lambda k: k['impact'], reverse=True)
-
-            # Put some other useful data into context.
-            result['name'] = url
-            result['url'] = result['google']['id']
-            result['id'] = url.replace(' ', '-')
-            result['rules_by_impact'] = rules_by_impact[0:7]
-            result['google']['score_history'] = score_history
-            result['wpt']['plt_history'] = plt_history
+                result = {
+                    'google': GoogleResultData(url),
+                    'wpt': WptResultData(url)
+                }
+            except NoDataError:
+                logging.info("There are no full results yet for %s" % url)
+                continue
 
             context['results'].append(result)
 
---
+++ testresult.py
@@ -0,0 +1,78 @@
+from datetime import date, timedelta
+import logging
+
+from django.utils import simplejson as json
+
+import models
+from utils import xml_to_json
+
+class NoDataError(Exception):
+    pass
+
+class ResultData(object):
+
+    def __init__(self, url, provider):
+
+        # Query for last 10 days of results.
+        q = models.TestResult.all()
+        q.filter('url =', url).filter('provider =', provider).filter('results_received', True)
+        q.filter('dt >', date.today() - timedelta(days=10))
+        q.order('dt')
+        self.tests = q.fetch(20)
+
+        if len(self.tests) < 1:
+            raise NoDataError
+
+        self.url = url
+        self.provider = provider
+
+        self.tests = self.parse_raw_result_data()
+        self.history = self.get_test_history(self.tests)
+        self.current = self.tests[-1]
+    
+    def parse_raw_result_data(self, results):
+        return results
+
+    def get_test_history(self, tests):
+        """
+        Dedupe results based on date.
+        """
+        history = []
+        for test in tests:
+            if history == [] or test.dt.date() != history[-1].dt.date():
+                history.append(test)
+        return history
+
+class GoogleResultData(ResultData):
+
+    def __init__(self, url):
+        super(GoogleResultData, self).__init__(url, 'google')
+        self.rules_by_impact = self.get_rules_by_impact()
+
+    def parse_raw_result_data(self):
+        parsed = []
+        for test in self.tests:
+            test._result = json.loads(test.result)
+            parsed.append(test)
+        return parsed
+
+    def get_rules_by_impact(self):
+        rules_by_impact = []
+        for rule_result in self.current.result['formattedResults']['ruleResults'].values():
+            rules_by_impact.append({
+                'name': rule_result['localizedRuleName'],
+                'impact': rule_result['ruleImpact']
+            })
+        return sorted(rules_by_impact, key=lambda k: k['impact'], reverse=True)[0:7]
+
+class WptResultData(ResultData):
+
+    def __init__(self, url):
+        super(WptResultData, self).__init__(url, 'wpt')
+
+    def parse_raw_result_data(self):
+        parsed = []
+        for test in self.tests:
+            test._result = xml_to_json(test.result)
+            parsed.append(test)
+        return parsed
