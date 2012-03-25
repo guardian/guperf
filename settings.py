a9e2adaa6e24f3732ecd0288d81e1f4aa74dcbad
Andy Hume <andyhume@gmail.com>
2012-03-25 18:05:04 +0100

    Added safe settings file.

---
+++ settings.py
@@ -0,0 +1,6 @@
+google_url = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed'
+wpt_url = 'http://www.webpagetest.org/runtest.php'
+wpt_results_url = 'http://www.webpagetest.org/xmlResult'
+
+google_api_key = '[your_key_here]'
+wpt_api_key = '[your_key_here]'
